63ca55d5793f951c85df7517cd786e24c468a3ec
JesseAraujo <jesse.araujo4@fatec.sp.gov.br>
2020-05-15 19:00:11 -0300

    Front-End

--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,32 @@
-window.onload = function(e) {
-    var offset = document.getElementsByClassName('header')[0].offsetTop;
-    var menu = document.getElementsByClassName('header')[0];
-
-    document.addEventListener('scroll', function() {
-        if (document.body.scrollTop > offset || document.documentElement.scrollTop > offset) {
-            menu.id = 'fixed'
-        } else {
-            menu.id = ''
-        }
+window.onload = function (e) {
+    $(function(){   
+        var nav = $('.header');   
+        $(window).scroll(function () { 
+            if ($(this).scrollTop() > 0) { 
+                nav.addClass("fixed");
+                $('body').css('padding-top', 150);
+            } else { 
+                nav.removeClass("fixed"); 
+                $('body').css('padding-top', 0);
+            } 
+        });  
     });
+}
+
+function mascaratel(telefone) {
+    if (telefone.value.length == 0)
+        telefone.value = '(' + telefone.value;
+    if (telefone.value.length == 3)
+        telefone.value = telefone.value + ') ';
+    if (telefone.value.length == 9)
+        telefone.value = telefone.value + '-';
+}
+
+function mascaracel(celular) {
+    if (celular.value.length == 0)
+        celular.value = '(' + celular.value;
+    if (celular.value.length == 3)
+        celular.value = celular.value + ') ';
+    if (celular.value.length == 10)
+        celular.value = celular.value + '-';
 }
