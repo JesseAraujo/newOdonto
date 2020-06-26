dde1af382d2a95387f9d5b2777370b95fd97d4f7
JesseAraujo <jesse.araujo4@fatec.sp.gov.br>
2020-06-26 12:12:57 -0300

    INSERT | DELETE incluÍdo no servidor

--- web/src/database/db.js
+++ web/src/database/db.js
@@ -2,61 +2,71 @@ const sqlite3 = require("sqlite3").verbose()
 
 const db = new sqlite3.Database("./src/database/database.db")
 
-db.serialize(() => {
+module.exports = db
+
+//db.serialize(() => {
     //criar tabela
-    db.run(`
-        CREATE TABLE IF NOT EXISTS schedules (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT,
-            cpf TEXT,            
-            email TEXT,
-            phone TEXT,
-            date TEXT,
-            month TEXT,
-            hour TEXT,
-            subject TEXT
-        );
-    `)
+    //db.run(`
+    //    CREATE TABLE IF NOT EXISTS schedules (
+    //        id INTEGER PRIMARY KEY AUTOINCREMENT,
+    //        name TEXT,
+    //        cpf TEXT,            
+    //        email TEXT,
+    //        phone TEXT,
+    //        date TEXT,
+    //        month TEXT,
+    //        hour TEXT,
+    //        subject TEXT
+    //    );
+    //`)
 
-    const query = `
-        INSERT INTO schedules (
-            name,
-            cpf,
-            email,
-            phone,
-            date,
-            month,
-            hour,
-            subject
-        ) VALUES (
-            ?, ?, ?, ?, ?, ?, ?, ?
-        )
-    `
-    const values = [
-        "Jessé Brisola de Araujo",
-        "440.697.878-01",
-        "jesse.br@hotmail.com",
-        "(14) 99624-7077",
-        "10",
-        "6",
-        "13h00",
-        "Agendamento para avaliação"
-    ]
 
-    function afterisertData(err) {
-        if (err) {
-            return console.log(err)
-        }
+   //inserir 
+   // const query = `
+   //     INSERT INTO schedules (
+   //         name,
+   //         cpf,
+   //         email,
+   //         phone,
+   //         date,
+   //        month,
+   //         hour,
+   //         subject
+   //    ) VALUES (
+   //         ?, ?, ?, ?, ?, ?, ?, ?
+   //     )
+   // `
+    //const values = [
+    //    "Jessé Brisola de Araujo",
+    //    "440.697.878-01",
+    //    "jesse.br@hotmail.com",
+    //    "(14) 99624-7077",
+    //    "10",
+    //    "6",
+    //    "13h00",
+    //   "Agendamento para avaliação"
+    //]
 
-        console.log("Cadastrado com sucesso!")
-        console.log(this)
-    }
+    //function afterisertData(err) {
+    //    if (err) {
+    //        return console.log(err)
+    //    }
 
-    db.run(query, values, afterisertData)
+    //    console.log("Cadastrado com sucesso!")
+    //    console.log(this)
+    //}
 
-    //inserir 
+    //db.run(query, values, afterisertData)
 
     //consultar
+    //db.all(`SELECT * FROM schedules`, function(err, rows) {
+    //    if (err) {
+    //        return console.log(err)
+    //    }
+
+    //    console.log("Aqui estão os registros:")
+    //    console.log(rows)
+    //})
 
     //deletar
-})
+//})
--- web/src/server.js
+++ web/src/server.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const server = express()
 
+//exportar db
+const db = require("./database/db")
 
 //pasta publica
 server.use(express.static("public"))
+
+//habilitar o uso do req.body na nossa aplicação
+server.use(express.urlencoded({ extended: true }))
 
 
 //template engine
@@ -14,7 +19,7 @@ nunjucks.configure("src/views", {
 })
 
 
-//rotas
+//rotas da páginas
 server.get("/", (req, res) => {
     return res.render(__dirname + "/views/index.html")
 })
@@ -31,6 +36,101 @@ server.get("/Contato", (req, res) => {
     return res.render(__dirname + "/views/contato.html")
 })
 
+
+//-------------------------------------------------------
+//
+server.post("/Agendamento", (req, res) => {
+
+    const subject = 'Agendamento de Avaliação'
+
+    //inserir dados no banco de dados
+    const query = `
+         INSERT INTO schedules (
+            name,
+            cpf,
+            email,
+            phone,
+            date,
+            month,
+            hour,
+            subject
+         ) VALUES ( ?, ?, ?, ?, ?, ?, ?, ? );    
+     `
+
+    const values = [
+        req.body.name,
+        req.body.cpf,
+        req.body.email,
+        req.body.phone,
+        req.body.day,
+        req.body.month,
+        req.body.hour,
+        subject
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            console.log(err)
+            return res.send("Erro no cadastro!")
+            //return res.render("create-point.html", { err: true })
+        }
+        console.log('Cadastrado com sucesso')
+        console.log(this)
+
+        return res.render(__dirname + "/views/index.html")
+    }
+
+    db.run(query, values, afterInsertData)
+})
+
+//-------------------------------------------------------
+//
+server.get("/MeusAgendamentos", (req, res) => {
+
+    db.all(`SELECT * FROM schedules`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+        
+
+        return res.render(__dirname + "/views/meuAgendamento.html", { schedules: rows, total })
+    })
+
+})
+
+server.get("/MeusAgendamentos/:id", (req, res) => {
+    const  id  = req.params.id
+
+    db.all(`SELECT * FROM schedules WHERE id =?`,[id], function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+        console.log(id)
+
+        return res.render(__dirname + "/views/meuAgendamento.html", { schedules: rows, id: id })
+    })
+
+})
+
+//-------------------------------------------------------
+//
+server.get("/MeuAgendamento/:id", function (req, res) {
+    const  id  = req.params.id
+
+    db.run("DELETE FROM schedules WHERE id =?",[id], function(err) {
+        if (err){
+             return res.send("Erro no banco de dados!")
+         }
+         //return res.redirect("/search")
+         return res.render(__dirname + "/views/index.html")
+    })
+
+})
 
 //iniciando servidor
 server.listen(3000)
